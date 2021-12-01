6e497f19ad24eacb048100ee2e166030a9c453bf
Bas Bakx <bas@owow.io>
2021-12-01 19:13:11 +0100

    day1

---
+++ day1/script.py
@@ -0,0 +1,22 @@
+with open('/Users/bas/AoC/day1/input.txt') as file:
+    lines = [int(x) for x in file.read().split()]
+
+x = 0
+
+for i, val in enumerate(lines):
+    if val > lines[i-1]:
+        x += 1
+
+y = 0
+window = 0
+lastWindow = 0
+for i, val in enumerate(lines):
+    if i < ( len(lines) - 2 ):
+        window = (lines[i] + lines[i+1] + lines[i+2])
+        if i > 0:
+            if window > lastWindow:
+                y += 1
+            lastWindow = window
+
+print("Answer 1:", x)
+print("Answer 2:", y)
