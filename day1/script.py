371e6978564ef6c1bd61967172e8226e46f822ec
Bas Bakx <bas@owow.io>
2021-12-01 19:18:08 +0100

    clean up code

--- day1/script.py
+++ day1/script.py
@@ -1,15 +1,15 @@
+x = 0
+y = 0
+window = 0
+lastWindow = 0
+
 with open('/Users/bas/AoC/day1/input.txt') as file:
     lines = [int(x) for x in file.read().split()]
-
-x = 0
 
 for i, val in enumerate(lines):
     if val > lines[i-1]:
         x += 1
 
-y = 0
-window = 0
-lastWindow = 0
 for i, val in enumerate(lines):
     if i < ( len(lines) - 2 ):
         window = (lines[i] + lines[i+1] + lines[i+2])
