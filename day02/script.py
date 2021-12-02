8ac965f44a5011a5019c4ebf5cbfa1ac41856ef0
Bas Bakx <bas@owow.io>
2021-12-02 09:48:16 +0100

    clean up

--- day02/script.py
+++ day02/script.py
@@ -7,16 +7,16 @@ pos = 0
 d = {'forward': 0, 'up': 0, 'down': 0}
 f = open('/Users/bas/aoc2021/day02/input.txt', 'r')
 for line in f.readlines():
-    name,score = line.split()
-    s = int(score)
-    l.append([name,s])
-    d[name] += int(s)
-    if name == 'down':
-        aim += s
-    elif name == 'up':
-        aim -= s
-    elif name == 'forward':
-        pos += aim * s
+    n,v = line.split()
+    v = int(v)
+    l.append([n,v])
+    d[n] += int(v)
+    if n == 'down':
+        aim += v
+    elif n == 'up':
+        aim -= v
+    elif n == 'forward':
+        pos += aim * v
 
 x = d['forward'] * (d['down'] - d['up'])
 y = d['forward'] * pos
