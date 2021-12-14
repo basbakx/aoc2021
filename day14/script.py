b3ceb83672a3e83c6e61a8648fd13d2cd46a57c3
Bas Bakx <bas@owow.io>
2021-12-14 21:24:47 +0100

    day14 part 2

--- day14/script.py
+++ day14/script.py
@@ -1,56 +1,79 @@
-import re
 f = open('/Users/bas/aoc2021/day14/input.txt', 'r')
 
+# Process input into dictionary of rules and list of pairs
 d = {}
-p = ''
+p = []
 
-# Setup rules
 for val in f:
     val = val.strip()
     if '->' in val:
         c, v = val.split(' -> ')
         d[c] = v
     elif val != '':
-        p = val
+        p = list(val)
 
-# largeString = "Lorem ipsum ###### some previous text to be erased ###### dolor sit amet"
-# smallString = "foo bar"
-# find = '######'
-# print(re.sub(rf'{find}', smallString, largeString))
 
-# def run(st):
-#     res = ''
-#     for key in d:
-#         r = ''
-#         re.sub(rf'{key}', key + d[key], r)
-#         res += r
-#     return res
-        
+# Create dictionary of initial letter pairs
+pairs = {}
 
-# for i in range(1):
-#     p = run(p)
-    
+for i in range(len(p) - 1):
+    pair = p[i] + p[i + 1]
+    if pair in pairs:
+        pairs[pair] += 1
+    else:
+        pairs[pair] = 1
+pairs[p[-1]] = 1
 
-def run(st):
-    li = list(st)
-    re = ''
-    for i in range(len(li) - 1):
-        pair = li[i] + li[i+1]
-        if pair in d:
-            re += li[i] + d[pair]
+
+# Create stepping function
+def step(pairs):
+    nPairs = {}
+
+    def add(val, count):
+        if val in nPairs:
+            nPairs[val] += count
+        else:   
+            nPairs[val] = count
+
+    for v in pairs:
+        count = pairs[v]
+        if v in d:
+            first = v[0] + d[v]
+            second = d[v] + v[1]
+            add(first, count)
+            add(second, count)
         else:
-            re += li[i]
-    re += li[-1]
-    return re
+            add(v, count)
 
-for i in range(40):
-    p = run(p)
-    print(i)
+    return nPairs
 
-print(len(p))
-print(set(p))
 
-xr = []
-for val in set(p):
-    xr.append(p.count(val))
-print(max(xr) - min(xr))
+# Solve function
+def solve():
+    xl = 0
+    res = {}
+    for val in pairs:
+        xl += pairs[val]
+        if val[0] in res:
+            res[val[0]] += pairs[val]
+        else:
+            res[val[0]] = pairs[val]
+
+    res = res.values()
+    x = max(res) - min(res)
+
+    return x
+
+
+# Solve 1
+for i in range(10): pairs = step(pairs)
+x = solve()
+
+
+# Solve 2
+for i in range(30): pairs = step(pairs)
+y = solve()
+
+
+print("Answer 1:", x)
+print("Answer 2:", y)
